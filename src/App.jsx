e7c7694f286534213c249936304ebba04d07042c
val <vallentenico21@gmail.com>
2026-09-03 16:37:36 +0800

    add functionality to input and submit button

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,24 @@
+import { useState } from 'react'
 import './App.css'
 
 export default function App() {
+
+  const [task, setTask] = useState("")
+
+  const [todos, setTodos] = useState([])
+  
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    setTodos(prevTodos => ([...prevTodos, task]))
+    setTask("")
+  }
+
+  const handleChange = (event) => {
+    setTask(event.target.value)
+  }
+
+  console.log(todos)
+  
   return (
     <>
       <header>
@@ -9,14 +27,18 @@ export default function App() {
 
       <main>
         <section className="todo-input">
-          <h2>Add a Task</h2>
-          <label htmlFor="todo">
+          <h2>Add Task</h2>
+          <form onSubmit={handleSubmit}>
+            <label htmlFor="todo" className="sr-only">Add a Task</label>
             <input 
               id="todo"
               type="text"
+              value={task}
+              onChange={handleChange}
               placeholder="Enter a task..."
             />
-          </label>
+            <button type="submit">Submit</button>
+          </form>
         </section>
 
         <section className="todo-display">
